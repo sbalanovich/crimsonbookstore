3a0ecde85850f6730244599bb654e282bd1d73e5
Serguei Balanovich <sbalanovich@college.harvard.edu>
2013-01-19 21:04:45 -0500

    Latest Commit
    Updated README and left in basic shell files

--- html/buy_profile_1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    // configuration
-    require("../includes/config.php"); 
-
-    // render portfolio
-    render("profile_form.php", ["title" => "book"]);
-
-?>
--- html/sell.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    // configuration
-    require("../includes/config.php"); 
-
-    // render portfolio
-    render("sell_0_form.php", ["title" => "main"]);
-
-?>
