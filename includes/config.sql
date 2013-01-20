cbddbfca8b6e9c16b5bb7eeaead72c27fe572b0e
Serguei Balanovich <sbalanovich@college.harvard.edu>
2013-01-20 15:40:10 -0500

    Added .sql files

--- includes/config.sql
+++ includes/config.sql
@@ -1,22 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
 --
--- Database: `project`
+-- Host: localhost
+-- Generation Time: Jan 20, 2013 at 03:38 PM
+-- Server version: 5.5.28
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
- 
-CREATE DATABASE IF NOT EXISTS  `project` ;
- 
- 
+-- Database: `crimsonbookstore`
 --
--- Table structure for table `users`
+
+-- --------------------------------------------------------
+
 --
- 
-CREATE TABLE IF NOT EXISTS `project`.`users` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `hash` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
- 
+-- Table structure for table `books`
 --
--- Dumping data for table `project`.`users`
+
+CREATE TABLE IF NOT EXISTS `books` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_email` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `required` int(1) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `condition` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
 --
+-- Table structure for table `user_cart`
+--
+
+CREATE TABLE IF NOT EXISTS `user_cart` (
+  `user` varchar(255) NOT NULL,
+  `bookid` int(10) NOT NULL,
+  `star` int(1) NOT NULL,
+  `cart` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
