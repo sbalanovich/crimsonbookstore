cf5b6a6f75c39a409638e24fd08d814c849e0437
Serguei Balanovich <sbalanovich@college.harvard.edu>
2013-01-19 23:15:13 -0500

    Commit2 Sergs
    Don't mind this commit, just another test

--- includes/functions.php
+++ includes/functions.php
@@ -16,7 +16,7 @@
      */
     function apologize($message)
     {
-        render("apology.php", ["message" => $message]);
+        render("apology.php", array("message" => $message));
         exit;
     }
 
