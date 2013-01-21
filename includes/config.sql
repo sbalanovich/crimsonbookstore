0bc01be5c36d66240cef5a07b185bad65e6969ab
Serguei Balanovich <sbalanovich@college.harvard.edu>
2013-01-20 21:41:55 -0500

    New .sql

--- includes/config.sql
+++ includes/config.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 3.5.2.2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 20, 2013 at 11:27 PM
--- Server version: 5.5.25
--- PHP Version: 5.4.4
+-- Generation Time: Jan 20, 2013 at 09:40 PM
+-- Server version: 5.5.28
+-- PHP Version: 5.4.7
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `crimsonbookstore`
@@ -20,18 +26,64 @@ SET time_zone = "+00:00";
 -- Table structure for table `books`
 --
 
-CREATE TABLE `books` (
+CREATE TABLE IF NOT EXISTS `books` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `user_email` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
-  `class` varchar(255) NOT NULL,
-  `required` int(1) NOT NULL,
+  `publisher` varchar(255) NOT NULL,
+  `class_id` varchar(10) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `summary` text NOT NULL,
+  `ISBN-10` varchar(11) NOT NULL,
+  `ISBN-13` varchar(14) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes`
+--
+
+CREATE TABLE IF NOT EXISTS `classes` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `short_name` varchar(255) NOT NULL,
+  `long_name` text NOT NULL,
+  `professor` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `listings`
+--
+
+CREATE TABLE IF NOT EXISTS `listings` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `book_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
   `price` varchar(255) NOT NULL,
   `condition` varchar(255) NOT NULL,
   `description` text NOT NULL,
+  `user_img` varchar(255) NOT NULL,
+  `contenders` int(10) NOT NULL,
+  `copies` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -39,9 +91,9 @@ CREATE TABLE `books` (
 -- Table structure for table `user_cart`
 --
 
-CREATE TABLE `user_cart` (
-  `user` varchar(255) NOT NULL,
-  `bookid` int(10) NOT NULL
+CREATE TABLE IF NOT EXISTS `user_cart` (
+  `user_id` bigint(10) NOT NULL,
+  `book_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -50,7 +102,11 @@ CREATE TABLE `user_cart` (
 -- Table structure for table `user_starred`
 --
 
-CREATE TABLE `user_starred` (
-  `user` varchar(255) NOT NULL,
-  `bookid` int(10) NOT NULL
+CREATE TABLE IF NOT EXISTS `user_starred` (
+  `user_id` int(10) NOT NULL,
+  `book_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
