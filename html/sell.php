093ea85b22de45d7d490cf87583eec8b7cfc0fbf
Lauren Urke <laurenurke@collge.harvard.edu>
2013-01-22 16:33:22 -0500

    sellform hidden presets and display sellform presets

--- html/sell.php
+++ html/sell.php
@@ -3,7 +3,32 @@
     // configuration
     require("../includes/config.php");
     
-    // render main
-    render("sell_form.php", array("title" => "Welcome to Crimson Bookstore!"));
+    // if form was submitted
+    //if (isset($_POST['sellformsubmit'])))
+    {
+        /*// validate submission do this with jquery
+        if (empty($_POST["username"]))
+        {
+            apologize("You must provide your username.");
+        }
+        else if (empty($_POST["password"]))
+        {
+            apologize("You must provide your password.");
+        }*/
+
+        //insert book and listing into database
+      //  listing=array();
+       // $_POST["price"]
+
+
+        // render sell
+       // render("sell_form.php",array("title" => "Welcome to Crimson Bookstore!"));
+
+    }
+   // else
+    {
+        // render sell
+    	render("sell_form.php", array("title" => "Welcome to Crimson Bookstore!"));
+    }
     
 ?>
--- templates/sell_form.php
+++ templates/sell_form.php
@@ -23,19 +23,21 @@
                 $('div.sellresult').remove();    
                 var query= $('#booksearchinput').val();
                 var xhr = new XMLHttpRequest();
-                xhr.open("GET", "https://www.googleapis.com/books/v1/volumes?q=" + query, false);
+                xhr.open("GET", "https://www.googleapis.com/books/v1/volumes?q=" + query +"&maxResults=20", false);
                 xhr.send();
                 var json = JSON.parse(xhr.responseText);
                 for(var i=0; i < json.items.length; i++) {
                     books[i]={};
-                    books[i]["authors"]=json.items[i].volumeInfo.authors;
+                    books[i]["authors"]=json.items[i].volumeInfo.authors.join(", ");
                     books[i]["title"]=json.items[i].volumeInfo.title;
-                    books[i]["subtitle"]=json.items[i].volumeInfo.subtitle;
+                    if (json.items[i].volumeInfo.subtitle != undefined)
+                        books[i]["title"]=books[i]["title"] + ": " + json.items[i].volumeInfo.subtitle;
                     books[i]["publisher"]=json.items[i].volumeInfo.publisher;
-                    books[i]["industryIdentifiers"]=json.items[i].volumeInfo.industryIdentifiers[1].identifier;
+                    books[i]["isbn13"]=json.items[i].volumeInfo.industryIdentifiers[1].identifier;
+                    books[i]["pic"]=json.items[i].volumeInfo.imageLinks.thumbnail;
 
                     //put search results into element
-                    $('<div class="sellresult" id=' + i + '>' + books[i]["industryIdentifiers"] + '</div>').appendTo('#sellsearchresults');
+                    $('<div class="sellresult" id=' + i + '>' + books[i]["title"] + '</div>').appendTo('#sellsearchresults');
                     }
                 });
             
@@ -44,29 +46,40 @@
                 var id= this.id;
                 $('#sellsearchresults').toggle();
                 $('#sellform').toggle();
-                $('<div class="sellresult" id=' + id + '>' + books[id]["authors"] + '</div>').prependTo('#sellform');
+                $("<input class='preset' value='" + books[id]["title"] + ": " + books[id]["subtitle"] + "' readonly>").prependTo('#sellform'); 
+                $("<input class='preset' value='" + books[id]["authors"] + "' readonly>").prependTo('#sellform'); 
+                $("<input class='preset' value='" + books[id]["publisher"] + "' readonly>").prependTo('#sellform'); 
+                $("<input class='preset' value='" + books[id]["isbn13"] + "' readonly>").prependTo('#sellform'); 
+                $('input.preset').toggle();
+
+                $("<li>" + books[id]["isbn13"] + " </li>").prependTo('#presetform');
+                $("<li>" + books[id]["publisher"] + " </li>").prependTo('#presetform');
+                $("<li>" + books[id]["authors"] + " </li>").prependTo('#presetform');
+                $("<li>" + books[id]["title"] + " </li>").prependTo('#presetform');
+                $("<img src='" + books[id]["pic"] + "' alt='book image'>").prependTo('#presetform');
+
             });
         });
         </script>
     </div>
     <div id="sellsearchresults">
     </div>
-
-//??? ask if should use post with real form and php or js, jquery, ajax
-
-
-    <div id="sellform">
-        <input id="price" placeholder="List your price" type="float"/>
-        <select id="condition" class="input-large" size="1">
-            <option value="" class="uneditable-input" selected="selected">Condition</option>
-            <option value="new">New</option>
-            <option value="excellent">Excellent</option>
-            <option value="good">Good</option>
-            <option value="fair">Fair</option>
-            <option value="poor">Poor</option>
-            <option value="damaged">Damaged</option>
-        </select>
-        <textarea class="input-xxlarge" placeholder="Description" maxlength="200" rows="3"></textarea>
-        <button id="sellformsubmit" class="btn">Search</button>
+    <div id="sellformdiv">
+        <ol id="presetform">
+        </ol>
+        <form id="sellform" name="sellform" method="post" action="sell.php">
+            <input name="price" placeholder="List your price" type="float"/>
+            <select name="condition" class="input-large" size="1">
+                <option value="" class="uneditable-input" selected="selected">Condition</option>
+                <option value="new">New</option>
+                <option value="excellent">Excellent</option>
+                <option value="good">Good</option>
+                <option value="fair">Fair</option>
+                <option value="poor">Poor</option>
+                <option value="damaged">Damaged</option>
+            </select>
+            <textarea name="description" class="input-xxlarge" placeholder="Description" maxlength="200" rows="3"></textarea>
+            <button type="submit" name="sellformsubmit" name="sellformsubmit" class="btn">Submit</button>
+        </form>
     </div>
 </div>
