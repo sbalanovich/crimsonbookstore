8488bc33b5401a65e9032e653f072511a7b3e2a8
Lauren Urke <laurenurke@collge.harvard.edu>
2013-01-21 22:52:58 -0500

    added comments, queried other fields from google api

--- templates/sell_form.php
+++ templates/sell_form.php
@@ -14,9 +14,12 @@
         <input id="booksearchinput" name="booksearch" placeholder="Search by Title, Author, or ISBN" type="text"/>
         <button id="sellsearchsubmit" class="btn">Search</button>
         <script> $(document).ready(function() {
+            //turn off form
             $('#sellform').toggle();
+
+            //query google books api on button clicked
             var books = {};
-            $('#sellsearchsubmit').click(function() { //onClick("functionname")
+            $('#sellsearchsubmit').click(function() {
                 $('div.sellresult').remove();    
                 var query= $('#booksearchinput').val();
                 var xhr = new XMLHttpRequest();
@@ -25,24 +28,33 @@
                 var json = JSON.parse(xhr.responseText);
                 for(var i=0; i < json.items.length; i++) {
                     books[i]={};
-                    books[i]["author"]=json.items[i].volumeInfo.authors[0];
+                    books[i]["authors"]=json.items[i].volumeInfo.authors;
                     books[i]["title"]=json.items[i].volumeInfo.title;
+                    books[i]["subtitle"]=json.items[i].volumeInfo.subtitle;
+                    books[i]["publisher"]=json.items[i].volumeInfo.publisher;
+                    books[i]["industryIdentifiers"]=json.items[i].volumeInfo.industryIdentifiers[1].identifier;
 
-                    $('<div class="sellresult" id=' + i + '>' + books[i]["author"] + '</div>').appendTo('#sellsearchresults');
-                }
-            });
+                    //put search results into element
+                    $('<div class="sellresult" id=' + i + '>' + books[i]["industryIdentifiers"] + '</div>').appendTo('#sellsearchresults');
+                    }
+                });
             
+            //toggle off search results, toggle on form, put defined info into form
             $('#sellsearchresults').on('click', '.sellresult', function() {
                 var id= this.id;
                 $('#sellsearchresults').toggle();
                 $('#sellform').toggle();
-                $('<div class="sellresult" id=' + id + '>' + books[id]["author"] + '</div>').prependTo('#sellform');
+                $('<div class="sellresult" id=' + id + '>' + books[id]["authors"] + '</div>').prependTo('#sellform');
             });
         });
         </script>
     </div>
     <div id="sellsearchresults">
     </div>
+
+//??? ask if should use post with real form and php or js, jquery, ajax
+
+
     <div id="sellform">
         <input id="price" placeholder="List your price" type="float"/>
         <select id="condition" class="input-large" size="1">
@@ -55,5 +67,6 @@
             <option value="damaged">Damaged</option>
         </select>
         <textarea class="input-xxlarge" placeholder="Description" maxlength="200" rows="3"></textarea>
+        <button id="sellformsubmit" class="btn">Search</button>
     </div>
 </div>
