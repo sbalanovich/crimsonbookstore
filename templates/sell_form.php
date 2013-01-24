b33822470db627633bd0e13bc3fa6df9f2dd42c4
Lauren Urke <laurenurke@collge.harvard.edu>
2013-01-23 21:51:22 -0500

    Revert "sell_form queries google book api, created style.scss"
    This reverts commit cda95b72d056a29ae7ce400adb90096dce732bb6.

--- templates/sell_form.php
+++ templates/sell_form.php
@@ -10,50 +10,25 @@
 </div>
 
 <div id="centercontent">
-    <div>
-        <input id="booksearchinput" name="booksearch" placeholder="Search by Title, Author, or ISBN" type="text"/>
-        <button id="sellsearchsubmit" class="btn">Search</button>
-        <script> $(document).ready(function() {
-            $('#sellform').toggle();
-            var books = {};
-            $('#sellsearchsubmit').click(function() { //onClick("functionname")
-                $('div.sellresult').remove();    
-                var query= $('#booksearchinput').val();
-                var xhr = new XMLHttpRequest();
-                xhr.open("GET", "https://www.googleapis.com/books/v1/volumes?q=" + query, false);
-                xhr.send();
-                var json = JSON.parse(xhr.responseText);
-                for(var i=0; i < json.items.length; i++) {
-                    books[i]={};
-                    books[i]["author"]=json.items[i].volumeInfo.authors[0];
-                    books[i]["title"]=json.items[i].volumeInfo.title;
+    <legend id="welcomeuser" style="color:#000000">
+        <?php
 
-                    $('<div class="sellresult" id=' + i + '>' + books[i]["author"] + '</div>').appendTo('#sellsearchresults');
-                }
-            });
-            
-            $('#sellsearchresults').on('click', '.sellresult', function() {
-                var id= this.id;
-                $('#sellsearchresults').toggle();
-                $('#sellform').toggle();
-                $('<div class="sellresult" id=' + id + '>' + books[id]["author"] + '</div>').prependTo('#sellform');
-            });
-        });
-        </script>
+        print("Welcome, [INSERT USERNAME HERE].");
+        printf("\n");
+        
+        ?>
+        </legend> 
+        <h2 style="color:#000000;">Sell Books!</h2>
+    </br><br>
+    <form action="buy_0.php" method="GET">
+        <fieldset>
+            <div class="row">
+            <div class="span12">
+            <div class="control-group ">
+                <input id="booksearch" autofocus name="booksearch" placeholder="Enter Class Name or Find your Book by Author, Title, or ISBN" type="text"/>
+            </div>
+            </div>
+            </div>
+        </fieldset>
+    </form>
     </div>
-    <div id="sellsearchresults">
-    </div>
-    <div id="sellform">
-        <input id="price" placeholder="List your price" type="float"/>
-        <select id="condition" class="input-large" size="1">
-            <option value="" class="uneditable-input" selected="selected">Condition</option>
-            <option value="new">New</option>
-            <option value="excellent">Excellent</option>
-            <option value="good">Good</option>
-            <option value="fair">Fair</option>
-            <option value="poor">Poor</option>
-            <option value="damaged">Damaged</option>
-        </select>
-        <textarea class="input-xxlarge" placeholder="Description" maxlength="200" rows="3"></textarea>
-    </div>
-</div>
