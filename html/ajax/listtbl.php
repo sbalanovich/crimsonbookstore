7de8dc8fc8e01bf5641ce5a753d70a70e7b42ffb
Serguei Balanovich <sbalanovich@college.harvard.edu>
2013-01-24 22:31:08 -0500

    updated buy

--- html/ajax/listtbl.php
+++ html/ajax/listtbl.php
@@ -4,24 +4,27 @@
         require '../db/connect.php';
         $query = mysql_query("SELECT * FROM listings WHERE (`book_id` = " . $_POST['bookid'] . ")");            
         
-        echo('<tr><td colspan = 4><table id = ' . $_POST['bookid'] . ' style="color:#000000; padding:15px;"  width = "90%">');
+        echo('<tr><td colspan = 4 class = "invisible" id = book_' . $_POST['bookid'] . '><table id = "listings" style="color:#000000; padding:15px;">');
         if (mysql_num_rows($query) !== 0)
         {               
                 while($results = mysql_fetch_array($query))
                 {
-#                    $userquery = mysql_query("SELECT * FROM users WHERE (`id` = " . $_POST['user_id'] . ")");
+                    $userquery = mysql_query("SELECT * FROM users WHERE (`id` = " . $results['user_id'] . ")");
+                    $userresults = mysql_fetch_array($userquery);
                     
                     $id = $results['id'];
                     $price = $results['price'];
                     $cond = $results['book_condition'];
-#                    $firstname = mysql_fetch_array($userquery)['firstname'];
-#                    $lastname = mysql_fetch_array($userquery)['lastname'];
+                    $firstname = $userresults['firstname'];
+                    $lastname = $userresults['lastname'];
                     echo("<tr id = " . $id . ">");
                     echo("<td style=\"text-align:center;\">LOL</td>");                    
                     echo("<td style=\"text-align:center;\"><h5>" . $price . "</h5></td>");
                     echo("<td style=\"text-align:center;\"><h5>" . $cond . "</h5></a></td>");
-                    echo("<td style=\"text-align:center;\"><h5>LOL</h5></a></td>");
-                    echo("<td style=\"text-align:center;\"><h5>LOL</h5></a></td>");
+                    echo("<td style=\"text-align:center;\"><h5>" . $firstname . "</h5></a></td>");
+                    echo("<td style=\"text-align:center;\"><h5>" . $lastname . "</h5></a></td>");
+                    echo("<td id = star_" . $id . "><i class=\"icon-star icon-black\"></td>");
+                    echo("<td id = cart_" . $id . "><i class=\"icon-shopping-cart icon-black\"></td>");
                     echo("</tr>");
                 }
         }
